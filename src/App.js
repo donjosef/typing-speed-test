d8a321210406883aeb51cdc851482a179293a996
donjoe <giumontanaro@gmail.com>
2018-12-02 15:42:21 +0100

    Add header and style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header>
+           <h1>Test Your Typing Speed</h1>
+        </header>
       </div>
     );
   }
