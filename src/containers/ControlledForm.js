a78eeda9a3c20d8c5ed3c8526088a428d4b2b348
donjoe <giumontanaro@gmail.com>
2018-12-02 17:17:11 +0100

    Check match with the original text at any keyup event and style textarea appropriately

--- src/containers/ControlledForm.js
+++ src/containers/ControlledForm.js
@@ -19,15 +19,18 @@ resetTextArea = () => {
 }
 
     render() {
-      const {start} = this.props;
+      const {start, checkMatch, completed, partialMatch, error} = this.props;
+      const style = completed ? {border: '10px solid #34A853'} : partialMatch ? {border: '10px solid #4584EF'} : error ? {border: '10px solid #EA4335'} : null;
 
        return(
         <div className='textAreaContainer'>
             <textarea
+                style={style}
                 value={this.state.value}
                 onChange={this.handleTextChange}
                 placeholder='The clock start when you start typing.'
                 onKeyPress={start}
+                onKeyUp={checkMatch}
            ></textarea>
            <button onClick={this.resetTextArea}>Start Over</button>
         </div>
--- src/containers/TestArea.js
+++ src/containers/TestArea.js
@@ -6,8 +6,13 @@ class TestArea extends Component {
 
  state = {
    timerRunning: false,
-   timer: [0,0,0,0]
+   timer: [0,0,0,0],
+   partialMatch: false,
+   completed: false,
+   error: false,
+   originText: 'Seventy seven benevolent elephants.'
  }
+
   startTimer = (e) => {
     if(e.target.value.length === 0 && this.state.timerRunning === false) { //se non si controlla timerRunning dentro la condizione, si rischia di far partire l'interval ogni qualvolta ritorno a 0 e ricomincio a scrivere
         this.setState({
@@ -25,14 +30,54 @@ tick = () => {
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000)); //centesimi
     this.setState({timer});
 }
+
+checkMatch = (e) => {
+    const textEntered = e.target.value;
+    if(textEntered !== "") { //keyup event triggers before textarea will have a value
+        const partialMatch = this.state.originText.substring(0, textEntered.length);
+       if(textEntered === this.state.originText) {
+           clearInterval(this.ID);
+           this.setState({
+               completed: true,
+               error: false,
+               partialMatch: false
+           });
+       } else {
+           if(textEntered === partialMatch) {
+               this.setState({
+                   completed: false,
+                   partialMatch: true,
+                   error: false
+               });
+           } else {
+               this.setState({
+                   partialMatch: false,
+                   error: true,
+                   completed: false
+               });
+           }
+       }
+    } else {
+        this.setState({
+            completed: false,
+            partialMatch: false,
+            error: false
+        });
+    }
+}
+
   render() {
     return (
       <div className='mainContent'>
           <section className='textToMatch'>
-              <p>Sentence to test</p>
+              <p>{this.state.originText}</p>
           </section>
           <ControlledForm
-             start={this.startTimer}/>
+             start={this.startTimer}
+             checkMatch={this.checkMatch}
+             completed={this.state.completed}
+             partialMatch={this.state.partialMatch}
+             error={this.state.error}/>
           <Timer timer={this.state.timer}/>
       </div>
 
