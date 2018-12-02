a253a205bd84e7888555d0d076fa40f4144444e0
donjoe <giumontanaro@gmail.com>
2018-12-02 20:20:40 +0100

    Updates

---
+++ precache-manifest.2cd121cd3bdf65ff46646ef4d2a7c172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "7a3a9ee655b7f292d543",
+    "url": "/react-gh-pages/static/js/main.7a3a9ee6.chunk.js"
+  },
+  {
+    "revision": "26997e4f8439735a840d",
+    "url": "/react-gh-pages/static/js/1.26997e4f.chunk.js"
+  },
+  {
+    "revision": "7a3a9ee655b7f292d543",
+    "url": "/react-gh-pages/static/css/main.b765e8d7.chunk.css"
+  },
+  {
+    "revision": "65bd15f42fd11118dd140172681703e8",
+    "url": "/react-gh-pages/index.html"
+  }
+];
